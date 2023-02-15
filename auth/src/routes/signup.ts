2da7f408a2721fb6c8ec5c38699a6cf722170405
vonrimS <mr.mytab@gmail.com>
2023-02-15 12:38:11 +0100

    User creation added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -15,19 +16,31 @@ router.post('/api/users/signup',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ]
-    , (req: Request, res: Response) => {
+    , async (req: Request, res: Response) => {
         const errors = validationResult(req);
+
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
 
         const { email, password } = req.body;
 
-        console.log('Creating a user...');
-        throw new DatabaseConnectionError();
+        const existingUser = await User.findOne({ email: email });
 
-        res.send({});
+        if (existingUser) {
+            console.log('Email in use');
+            return res.send({});
+        }
 
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send(user);
+
+        // const { email, password } = req.body;
+        // console.log('Creating a user...');
+        // throw new DatabaseConnectionError();
+        // res.send({});
         // new User({email, password});
     });
 
