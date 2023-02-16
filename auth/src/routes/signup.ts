9a79aaf4de20bbaa3ce273591699a65b3289504f
vonrimS <mr.mytab@gmail.com>
2023-02-16 02:27:15 +0100

    Validate request middleware added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { User } from '../models/user';
 import { NotFoundError } from '../errors/not-found-error';
@@ -21,14 +21,9 @@ router.post('/api/users/signup',
             .trim()
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
-    ]
-    , async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
         const { email, password } = req.body;
 
         const existingUser = await User.findOne({ email: email });
@@ -47,7 +42,7 @@ router.post('/api/users/signup',
             id: user.id,
             email: user.email
         },
-        process.env.JWT_KEY!
+            process.env.JWT_KEY!
         );
 
         // Store it on session object
