137d32d5caf03e6063b027fbb319c9d6a1cd78ac
vonrimS <mr.mytab@gmail.com>
2023-02-18 01:08:37 +0100

    More tests added

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,38 @@ it('returns a 201 on successful signup', async () => {
         .expect(201);
 });
 
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test2@gmail.c1om",
+            password: "123456sadwwww"
+        })
+        .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+        .post('/api/users/signin')
+        .send({
+            email: "test2@gmail.c1om",
+            password: "123456sadwwwwWRONG"
+        })
+        .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test2@gmail.com"
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            password: "123456sadwwww"
+        })
+        .expect(400);
+});
