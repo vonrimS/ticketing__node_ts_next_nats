8023a33977d465340f8cce042493094422a19b13
vonrimS <mr.mytab@gmail.com>
2023-02-19 02:15:22 +0100

    Axios Signup req and Handling validation errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,23 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        console.log(email, password);
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            });
+            console.log(response.data);
+        } catch (error) {
+            // console.log(error.response.data);
+            setErrors(error.response.data.errors);
+        }
+
         setEmail('');
         setPassword('');
     };
@@ -29,6 +40,12 @@ export default () => {
                     onChange={e => setPassword(e.target.value)}
                     type="password" className="form-control" />
             </div>
+            {errors.length > 0 && (<div className="alert alert-danger">
+                <h4>Oooops...</h4>
+                <ul className="my-0">
+                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                </ul>
+            </div>)}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
