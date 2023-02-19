27cadd3fc4613a9108ac430306d9f02de50f709a
vonrimS <mr.mytab@gmail.com>
2023-02-19 16:46:51 +0100

    getInitialProps backup

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,12 +2,44 @@ import axios from 'axios';
 
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
-  return <h1>Landing Page</h1>;
+    //   console.log(currentUser);
+    //   axios.get('/api/users/currentuser').catch((err) => {
+    //     console.log(err.message);
+    //   });
+
+    console.log(currentUser);
+    return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+    // console.log(req.headers);
+    if (typeof window === 'undefined') {
+        const { data } = await axios.get(
+            // 'http://SERVICENAME.NAMESPACE.svc.cluster.local'
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: req.headers
+            }
+        );
+
+        return data;
+        // we are on the server!
+    } else {
+        // we are on the browser
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
+
+    console.log('I WAS EXECUTED');
+    return {};
 };
 
 
 export default LandingPage;
+
+
+// At the time of writing, the service name for all platforms
+// (Windows, Mac, Linux) and Docker clients (Docker Desktop
+//     and Minikube) should be:
+
+// ingress-nginx-controller
