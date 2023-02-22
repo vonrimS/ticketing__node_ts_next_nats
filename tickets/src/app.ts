8e4cdaf91c9f22370a3e2adc6c2c11bbae2a9ec7
vonrimS <mr.mytab@gmail.com>
2023-02-22 10:31:17 +0100

    testing Show routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 import { errorHandler, NotFoundError, currentUser } from '@von_ticket/common';
 
@@ -21,6 +22,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
