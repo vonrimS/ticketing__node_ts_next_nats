bf72b920c306f070941d6f93ee0f6c147c8a62b2
vonrimS <mr.mytab@gmail.com>
2023-02-26 15:30:38 +0100

    nats, enforcing data types

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,11 +4,12 @@ import { TicketCreatedEvent } from "./ticket-created-event";
 import {Subjects} from './subjects';
 
 
+
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
-    onMessage(data: any, msg: Message) {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message) {
         console.log('Event data!', data);
 
         msg.ack();
