cd663ac3ac82265d9f8a978fd69fdc12ac9cbbde
vonrimS <mr.mytab@gmail.com>
2023-02-26 15:26:00 +0100

    nats, enforcing listener subjects

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,9 +1,15 @@
 import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
-abstract class Listener {
-    abstract subject: string;
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+export abstract class Listener<T extends Event> {
+    abstract subject: T['subject'];
     abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void;
+    abstract onMessage(data: T['data'], msg: Message): void;
     private client: Stan;
     protected ackWait = 5 * 1000;
 
@@ -45,5 +51,3 @@ abstract class Listener {
             : JSON.parse(data.toString('utf8'));
     }
 }
-
-export { Listener };
--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,11 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from "./ticket-created-event";
+import {Subjects} from './subjects';
 
-class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
     onMessage(data: any, msg: Message) {
@@ -11,6 +14,3 @@ class TicketCreatedListener extends Listener {
         msg.ack();
     }
 }
-
-
-export { TicketCreatedListener };
