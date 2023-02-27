f70732633cb6a2e627d9aba1d88d39cb0f79520d
vonrimS <mr.mytab@gmail.com>
2023-02-27 13:55:10 +0100

    orders, project setup

---
+++ orders/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+
+import { errorHandler, NotFoundError, currentUser } from '@von_ticket/common';
+import { updateTicketRouter } from './routes/updates';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        // secure: true
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
