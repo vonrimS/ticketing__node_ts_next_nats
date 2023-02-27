bda55bef70315de0604b0a7adfe0073c8ccf5087
vonrimS <mr.mytab@gmail.com>
2023-02-27 02:11:57 +0100

    tickets, ensuring mock invocation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
@@ -84,6 +85,8 @@ it('creates a ticket with valid inputs', async () => {
     tickets = await Ticket.find({});
     expect(tickets.length).toEqual(1);
     expect(tickets[0].price).toEqual(20);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
 
 
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -10,7 +10,7 @@ declare global {
 }
 
 
-jest.mock('../nats-wrapper')
+jest.mock('../nats-wrapper');
 
 let mongo: any;
 
@@ -23,6 +23,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+    jest.clearAllMocks();
     const collections = await mongoose.connection.db.collections();
     for (let collection of collections) {
         await collection.deleteMany({});
