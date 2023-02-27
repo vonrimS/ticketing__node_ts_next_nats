946833f2a0e4889dc1b49947760427fc348dc94b
vonrimS <mr.mytab@gmail.com>
2023-02-27 14:49:19 +0100

    orders, scaffolding new routes handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
+
+import { deleteOrderRouter } from './routes/delete';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
 
 import { errorHandler, NotFoundError, currentUser } from '@von_ticket/common';
-import { updateTicketRouter } from './routes/updates';
 
 
 const app = express();
@@ -23,10 +24,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
